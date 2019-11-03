cbc2459e789b37e2706e0737ba687bba5b30e29c
Valentin Le Bon <lebonvalentin@hotmail.fr>
2019-11-03 20:30:08 +0100

    Add php Registration page

--- register.php
+++ register.php
@@ -1,6 +1,30 @@
 <?php
     if ($id!=0) erreur(ERR_IS_CO);
 
+if (isset($_POST['submit']))
+{
+    if(empty($_POST['Nom']) and empty($_POST['Prenom']) and empty($_POST['email']) and empty($_POST['password']) and empty($_POST['repeatpassword']))
+    {
+        if (strlen($password)>=6)
+        {
+            if ($password==$repeatpassword)
+            {
+                $password = md5($password);
+                $dbconnect = new mysqli ("localhost","root","","root"); 
+                $sql = "INSERT INTO newclient VALUES ('','$Username','email','$password')"; 
+                if (!$dbconnect->query($sql))
+                {
+                    printf("Message d'erreur : %s\n", $dbconnect->error);
+                }
+            // on ferme la connexion
+            mysqli_close($dbconnect); 
+            }
+            else echo "Les mots de passe ne sont pas identiques";
+        }
+        else echo "Le mot de passe est trop court !";
+    }
+    else echo "Veuillez saisir tous les champs !";  
+}   
 ?>
 
 <!DOCTYPE html>
@@ -27,7 +51,7 @@
 					<input class="text" type="text" name="Username" placeholder="Username" required="">
 					<input class="text email" type="email" name="email" placeholder="Email" required="">
 					<input class="text" type="password" name="password" placeholder="Mot de Passe" required="">
-					<input class="text w3lpass" type="password" name="password" placeholder="Retaper le mot de passe" required="">
+					<input class="text w3lpass" type="password" name="repeatpassword" placeholder="Retaper le mot de passe" required="">
 					<input type="submit" value="Inscription">
 				</form>
 				<p>Déja un compte ?<a href="login.php"> Connectez vous </a></p>
